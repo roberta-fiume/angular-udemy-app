6df0b1460e734330dc75666d9ed9ec13f71ba548
FIUME Roberta <roberta.fiume@hexagon.com>
2021-08-17 16:50:25 +0100

    string interpolation

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
+    // selector: '[app-servers]',
     selector: 'app-servers',
-    templateUrl: './servers.component.html'
+    templateUrl: './servers.component.html',
+    styleUrls: ['./servers.component.css']
 })
 
 export class ServersComponent {
