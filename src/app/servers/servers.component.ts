cfcd426ed6ea04194a1c11a13c39398f94552c12
FIUME Roberta <roberta.fiume@hexagon.com>
2021-08-20 17:11:41 +0100

    added dynamic classes

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -11,6 +11,7 @@ export class ServersComponent {
     allowNewServer = false;
     serverCreationStatus = "No server was created";
     serverName = "test server";
+    serverCreated = false;
 
     constructor() {}
 
@@ -21,6 +22,7 @@ export class ServersComponent {
     }
 
     onCreateServer() {
+        this.serverCreated = true;
         this.serverCreationStatus = "Server was created! Name is " + this.serverName;
     }
 
